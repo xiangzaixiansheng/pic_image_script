2892287442064379e8f8df7dc754bd98ed6b0129
hanxiang1 <hanxiang1@staff.sina.com.cn>
2025-06-13 11:46:47 +0800

    feat: 下载模型到指定路径

--- download_model.py
+++ download_model.py
@@ -1,4 +1,12 @@
 from modelscope.hub.snapshot_download import snapshot_download
-# 下载模型
-path = snapshot_download('damo/cv_unet_image-matting')
+import os
+
+# 指定本地保存目录
+project_root = os.path.dirname(os.path.abspath(__file__))
+save_dir = os.path.join(project_root, "model")
+
+# 下载模型到指定目录
+path = snapshot_download('maple775885/RMBG-2.0', cache_dir=save_dir)
+print("模型已下载到：", path)
+
 print(path)
